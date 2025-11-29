4370c64e48fc46f432194b00df0a3c2123c3dd36
sheena <syeongsn@gmail.com>
2025-11-29 16:42:30 +0800

    chore: installed and use general middleware

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import logger from 'morgan'
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const PORT = 3000;
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+  })
+);
+app.use(express.json());
+app.use(logger('dev'))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
