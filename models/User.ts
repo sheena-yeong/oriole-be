71e49368394b676b11c9196e8a737a9c580d20bf
sheena <syeongsn@gmail.com>
2025-12-01 23:10:12 +0800

    chore: created User model

---
+++ models/User.ts
@@ -0,0 +1,87 @@
+import { Model, DataTypes, Sequelize, Optional } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+interface UserAttributes {
+  id: number;
+  email: string;
+  password: string;
+  firstName?: string;
+  lastName?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// id, created and updated at is optional since it's auto-generated
+interface UserCreationAttributes extends Optional<UserAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
+
+// Extend the Sequlize Model class
+class User
+  extends Model<UserAttributes, UserCreationAttributes>
+  implements UserAttributes
+{
+  public id!: number;
+  public email!: string;
+  public password!: string;
+  public firstName?: string;
+  public lastName?: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  // Instance method to compare passwords
+  public async comparePassword(inputPassword: string): Promise<boolean> {
+    return bcrypt.compare(inputPassword, this.password);
+  }
+}
+
+export const initUserModel = (sequelize: Sequelize): typeof User => {
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [6, 100],
+        },
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'users',
+      timestamps: true,
+    }
+  );
+
+  // beforeCreate hook comes from Sequelize's Model class
+  User.beforeCreate(async (user: User) => {
+    if (user.password) {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  });
+
+  return User;
+};
+
+export { User, UserAttributes, UserCreationAttributes };
