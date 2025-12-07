2b74ab6f0442e80a2e1ee42a0389344c70a64efa
sheena <syeongsn@gmail.com>
2025-12-07 19:48:33 +0800

    chore: connect to redis during server start up

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import logger from 'morgan';
-import { verifyToken } from '../middleware/verifyToken';
+import { verifyToken } from './middleware/verifyToken';
 import dotenv from 'dotenv';
-import { connectDB } from '../db/db';
-import authRouter from '../routers/authRoutes';
-import cryptoRouter from '../routers/cryptoRoutes';
+import { connectDB } from './db/db';
+import authRouter from './routers/authRoutes';
+import cryptoRouter from './routers/cryptoRoutes';
+import { connectRedis, disconnectRedis } from './utils/redis';
 
 dotenv.config();
 const app = express();
@@ -24,6 +25,23 @@ app.use(logger('dev'));
 app.use('/auth', authRouter);
 app.use('/coins', verifyToken, cryptoRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectRedis();
+    console.log('Redis connected');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+}
+
+process.on('SIGINT', async () => {
+  await disconnectRedis();
+  process.exit(0);
+})
+
+startServer();
