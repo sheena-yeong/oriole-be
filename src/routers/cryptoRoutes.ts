b188d1920ca42111efcab637a412db158f079d0a
sheena <syeongsn@gmail.com>
2025-12-09 22:49:32 +0800

    chore: added routes for market chart and fear greed data

--- src/routers/cryptoRoutes.ts
+++ src/routers/cryptoRoutes.ts
@@ -5,11 +5,15 @@ import {
   addWatchListCoins,
   getWatchListCoins,
   deleteWatchListCoins,
+  getMarketChart,
+  getFearGreedLatest
 } from '../controllers/cryptoController';
 
 router.get('/', getCoins);
 router.post('/watchlist', addWatchListCoins);
 router.get('/watchlist', getWatchListCoins);
 router.delete('/watchlist', deleteWatchListCoins);
+router.get('/:id/market-chart', getMarketChart)
+router.get('/fear-greed', getFearGreedLatest)
 
 export default router;
