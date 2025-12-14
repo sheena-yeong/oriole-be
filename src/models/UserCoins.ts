bd37bb70c1ed93155a39d5e77a5e4e684af3495c
sheena <syeongsn@gmail.com>
2025-12-14 14:18:58 +0800

    feat: add UserCoins model

---
+++ src/models/UserCoins.ts
@@ -0,0 +1,69 @@
+import { Model, DataTypes, Sequelize, Optional } from 'sequelize';
+
+interface UserCoinAttributes {
+  id: number;
+  userId: number;
+  coinSymbol: string;
+  quantity: number;
+  buyPrice: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+interface UserCoinCreationAttributes
+  extends Optional<UserCoinAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
+
+class UserCoins
+  extends Model<UserCoinAttributes>
+  implements UserCoinAttributes
+{
+  public id!: number;
+  public userId!: number;
+  public coinSymbol!: string;
+  public quantity!: number;
+  public buyPrice!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+export const initUserCoinsModel = (sequelize: Sequelize): typeof UserCoins => {
+  UserCoins.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE', // if user is deleted, delete all coins too
+      },
+      coinSymbol: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      buyPrice: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'userCoins',
+      timestamps: true,
+    }
+  );
+  return UserCoins;
+};
+
+export { UserCoins, UserCoinCreationAttributes };
