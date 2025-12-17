d56e0565cfbbb4fed1f3c704b8005c3c9bd4c8ed
sheena <syeongsn@gmail.com>
2025-12-17 17:07:33 +0800

    chore: adjust sequelize settings, start and build scripts for railway

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,16 +6,27 @@ import { initUserCoinsModel } from '../models/UserCoins';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME!,
+      process.env.DB_USER!,
+      process.env.DB_PASSWORD!,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+      }
+    );
 
 const connectDB = async () => {
   try {
